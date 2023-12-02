4a58644113505e60e388acc06e19df4749f6f8df
朱靖骐 <201250049@smail.nju.edu.cn>
2023-12-02 17:29:48 +0800

    feat: improve ChatWindow's message display

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,41 +1,47 @@
 <template>
-  <div class="chat-window">
-    <!-- 显示聊天消息的容器 -->
-    <el-container class="message-container">
-      <el-header
-          style="margin-top: 50px;line-height:1.7;font: 20px Extra large"
+  <el-container class="el-container"
+                style="min-height: 100%;min-width: 100%; border: 1px solid #eee">
+    <div class="chat-window">
+      <!-- 显示消息的容器 -->
+      <el-container class="message-container" style="height: 540px">
+        <el-header
+            style="margin-top: 50px;line-height:1.7;font: 20px Extra large"
+            class="el-icon-question">
+          Stack Overflow 问答
+        </el-header>
+        <ul class="infinite-list" v-infinite-scroll="load"
+            style="height:400px;width:460px;overflow:auto">
+          <li v-for="message in messages" :key="message.id" class="message">
+            <div v-if="message.isMe" class="message-text mine">{{ message.text }}</div>
+            <div v-else class="message-text">{{ message.text }}</div>
+          </li>
+        </ul>
+      </el-container>
 
-          class="el-icon-question">
-        Stack Overflow 问答
-      </el-header>
-      <div v-for="message in messages" :key="message.id" class="message">
-        <div v-if="message.isMe" class="message-text mine">{{ message.text }}</div>
-        <div v-else class="message-text">{{ message.text }}</div>
-      </div>
-    </el-container>
-    <!-- 输入消息的表单 -->
-    <form class="input-form">
-      <el-input
-          v-model="inputText"
-          type="textarea"
-          :rows="2"
-          placeholder="Enter question..." />
-      <div style="margin-top: 5px">
-        <el-button
-            @click="sendMessage"
-            v-loading.fullscreen.lock="loading"
-            size="mini">
-          发送
-        </el-button>
-        <el-button
-            @click="test"
-            v-loading.fullscreen.lock="loading"
-            size="mini">
-          测试
-        </el-button>
-      </div>
-    </form>
-  </div>
+      <!-- 输入消息 -->
+      <form class="input-form" style="width: 500px;margin-left:10px;margin-bottom: 30px">
+        <el-input
+            v-model="inputText"
+            type="textarea"
+            :rows="2"
+            placeholder="Enter question..." />
+        <div style="margin-top: 5px">
+          <el-button
+              @click="sendMessage"
+              v-loading.fullscreen.lock="loading"
+              size="mini">
+            发送
+          </el-button>
+          <el-button
+              @click="test"
+              v-loading.fullscreen.lock="loading"
+              size="mini">
+            测试
+          </el-button>
+        </div>
+      </form>
+    </div>
+  </el-container>
 </template>
 
 <script>
@@ -46,10 +52,14 @@ export default {
     return {
       inputText: '',
       messages: [],
+      count:0,
       loading: false
     };
   },
   methods: {
+    load () {
+      this.count += 2
+    },
     sendMessage() {
       if (this.inputText.trim()) {
         this.loading = true;
@@ -82,7 +92,7 @@ export default {
 
 <style scoped>
 .chat-window {
-  max-width: 400px;
+  max-width: 600px;
   margin: 0 auto;
 }
 
@@ -91,13 +101,13 @@ export default {
 }
 
 .message {
-  padding: 5px;
-  margin-bottom: 5px;
+  padding: 3px;
+  margin-bottom: 3px;
 }
 
 .message-text {
-  padding: 10px;
-  border-radius: 5px;
+  padding: 5px;
+  border-radius: 3px;
 }
 
 .mine {
